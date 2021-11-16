5cacaf5352221d00face4278e89f828ffe5d0212
synopss <synopss@outlook.com>
2021-11-16 19:56:41 +0100

    Adding nicknames column & cleaning code

--- README.md
+++ README.md
@@ -14,24 +14,24 @@ Made in Python 3.9
 python main.py
 ```
 Then you will need to enter the tournament ID then wait the end of the process.
-If there are teams which don't meet the requirements, this table will show up, filled with problematic teams :
+If there are teams which don't meet the requirements, this table will show up, filled with problematic teams and the problematic players:
 
 ```
-+------------+-------------------+
-|  Team ID   |     Team Name     |
-+============+===================+
-| 1234567894 | Team 1            |
-+------------+-------------------+
-| 2131659894 | Team 2            |
-+------------+-------------------+
-| 7563249852 | Team 3            |
-+------------+-------------------+
-| 1324987324 | Team 4            |
-+------------+-------------------+
-| 7563219783 | Team 5            |
-+------------+-------------------+
-| 3295465231 | Team 6            |
-+------------+-------------------+
++------------+-------------------+-------------------+
+|  Team ID   |     Team Name     |     Nicknames     |
++============+===================+===================+
+| 1234567894 | Team 1            | nick1,nick2,nick3 |
++------------+-------------------+-------------------+
+| 2131659894 | Team 2            | nick4,nick2,nick7 |
++------------+-------------------+-------------------+
+| 7563249852 | Team 3            | nick8,nick5,nick1 |
++------------+-------------------+-------------------+
+| 1324987324 | Team 4            | nick2,nick3       |
++------------+-------------------+-------------------+
+| 7563219783 | Team 5            | nick9,nick2       |
++------------+-------------------+-------------------+
+| 3295465231 | Team 6            | nick3             |
++------------+-------------------+-------------------+
 ```
 
 ## Team requirements
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import texttable
 import urllib3
 from tqdm import tqdm
+from collections import Counter
 
 http = urllib3.PoolManager()
 
@@ -24,30 +25,27 @@ def check_teams(tournament_id):
         pbar = tqdm(total=total_count, ncols=100, unit='teams', desc='Processing: ')
         error_array = []
 
-        for results in data['data']['results']:
-            player_previous_clan = ''
-            for players in results['players']:
+        for team in data['data']['results']:
+            team_id = 0
+            players = []
+            for player in team['players']:
                 url = 'https://api.worldoftanks.eu/wot/clans/accountinfo/?application_id' \
-                      '=74613ef82f2d90e9c88a8449723936fe&account_id=' + players['uuid']
+                      '=74613ef82f2d90e9c88a8449723936fe&account_id=' + player['uuid']
                 data = get_data_response(url)
 
-                if data['data'][players['uuid']] is not None:
-                    player_clan = get_clan(data, players)
-                    if player_previous_clan == '':
-                        player_previous_clan = player_clan
-                    if player_previous_clan != player_clan:
-                        error_array = append_error_array(players['team_id'], results['title'], error_array)
-                        break
-                    player_previous_clan = player_clan
-                else:
-                    error_array = append_error_array(players['team_id'], results['title'], error_array)
-                    break
+                players.append({'player': player['nickname'], 'clan': get_clan_from_data(data, player)})
+                team_id = player['team_id']
+
+            problematic_players = get_problematic_players(players)
+            if problematic_players[0] != []:
+                error_array.append({'team_id': team_id, 'team_name': team['title'], 'nicknames': ','.join(problematic_players[0])})
+
             pbar.update(1)
         pbar.close()
 
         print_ending_message(error_array)
     else:
-        print("This tournament doesn't exist")
+        print('This tournament doesn\'t exist')
 
 
 def get_url(tournament_id, page_number):
@@ -65,32 +63,44 @@ def round_up(x):
     return int(math.ceil(x / 10.0)) * 10
 
 
-def get_clan(data, players):
-    return data['data'][players['uuid']]['clan']['tag']
+def get_clan_from_data(data, player):
+    if data['data'][player['uuid']] is not None:
+        return data['data'][player['uuid']]['clan']['tag']
+    return 'No clan'
 
 
-def append_error_array(team_id, team_name, input_array):
-    team_object = {'team_id': team_id, 'team_name': team_name}
+def get_problematic_players(players):
+    clan_counts = Counter(p['clan'] for p in players)
+    clan_from_team = clan_counts.most_common(1)[0][0]
+    return [p['player'] for p in players if p['clan'] != clan_from_team], clan_from_team
+
+
+def append_error_array(team_id, team_name, nicknames, input_array):
+    team_object = {'team_id': team_id, 'team_name': team_name, 'nicknames': nicknames}
     input_array.append(team_object)
     return input_array
 
 
 def print_error_table(teams):
     table = texttable.Texttable()
-    table.add_rows([["Team ID", "Team Name"]], True)
-    table.set_cols_dtype(['t', 'i'])
-    for i in teams:
-        table.add_row([i['team_id'], i['team_name']])
-    print(table.draw() + "\n")
+    table.add_rows([['Team ID', 'Team Name', 'Nicknames']], True)
+    table.set_cols_dtype(['i', 't', 't'])
+    for t in teams:
+        table.add_row([t['team_id'], t['team_name'], t['nicknames']])
+    print(table.draw() + '\n')
 
 
 def print_ending_message(error_array):
     if error_array is not None:
-        print("Those teams don't meet the requirements: ")
+        teams_count = len(error_array)
+        if teams_count == 1:
+            print('This team doesn\'t meet the requirements: ')
+        elif teams_count > 1:
+            print(f'Those {teams_count} teams don\'t meet the requirements: ')
         print_error_table(error_array)
     else:
         print('All teams meet the requirements')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
